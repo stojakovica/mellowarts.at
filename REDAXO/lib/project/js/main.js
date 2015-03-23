e03ab72d72e97b6f7368ca7ee73da8710238daeb
Aleksandar Stojakovic <aleks@mindstream.at>
2015-03-23 16:29:24 +0100

    Work on page

--- REDAXO/lib/project/js/main.js
+++ REDAXO/lib/project/js/main.js
@@ -19,6 +19,10 @@ $(document).ready(function() {
         $('body,html').animate({scrollTop:0},1000);
         return false;
     })
+
+    $('.gmapsContainer .overlay').click(function () {
+        $(this).remove();
+    });
 });
 
 function scrollToContent() {
